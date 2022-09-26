ac0092c72958a150a9eb3ff2bfbfcbe2f04cd367
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-09-26 22:36:50 +0300

    serializer

--- quiz/models.py
+++ quiz/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.,
-class UpdateCrateDate(models.Model):
+
+class UpdateCreateDate(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -9,47 +9,61 @@ class UpdateCrateDate(models.Model):
         abstract = True
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Category Name")
+class Category(models.Model):  # Categorys  --> ies
+    name = models.CharField(max_length=50, verbose_name='Category Name')
 
-    def __str__(self):  # Categorys --> ies
+    def __str__(self):
         return self.name
 
     class Meta:
         verbose_name_plural = 'Categories'
 
+    @property
+    def quiz_count(self):
+        return self.quizz.count()  # bACKEND ---> 15 quiz
 
-class Quiz(models.Model):
-    title = models.CharField(max_length=50, verbose_name='Quiz Title')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-    def __str__(self):  
+class Quiz(UpdateCreateDate):
+    title = models.CharField(max_length=50, verbose_name='Quiz title')
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='quizz')
+
+    def __str__(self):
         return self.title
 
     class Meta:
         verbose_name_plural = 'Quizzes'
 
+    @property
+    def question_count(self):
+        return self.question_set.count()
 
-class Question(models.Model):
 
-    SCALE = {
+class Question(UpdateCreateDate):
+
+    SCALE = (
         ('B', 'Beginner'),
         ('I', 'Intermediate'),
         ('A', 'Advanced')
-    }
+    )
 
     title = models.TextField()
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     difficulty = models.CharField(max_length=1, choices=SCALE)
 
-    def __str__(self):  
+    def __str__(self):
         return self.title
 
 
-class Option(models.Model):
+class Option(UpdateCreateDate):
     option_text = models.CharField(max_length=200)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='options')
     is_right = models.BooleanField(default=False)
 
-    def __str__(self):  
+    def __str__(self):
         return self.option_text
+
+
+
+
+        
